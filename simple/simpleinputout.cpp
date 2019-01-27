917f9bdf78af09e676823d7e2cb55ba4b764538e
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-01-27 18:13:28 +0600

    Create simpleinputout.cpp

---
+++ simple/simpleinputout.cpp
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?h_r=next-challenge&h_v=zen
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int sum=0;
+    sum=a+b+c;
+    cout<<sum<<endl;
+
+    return 0;
+}
